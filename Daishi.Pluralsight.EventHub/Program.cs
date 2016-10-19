bf99f144ea96cd6cbb142eff4147715b1f4cbef6
Paul Mooney <paul@daishisystems.com>
2016-10-19 23:20:28 +0100

    Initial commit

---
+++ Daishi.Pluralsight.EventHub/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// Clipboard Ring: ctrl, shift, v
+
+namespace Daishi.Pluralsight.EventHub
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
