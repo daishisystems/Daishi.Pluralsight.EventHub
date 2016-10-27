8ca70f333ea93aff85e50cf58f7f4d535a151e51
Paul Mooney <paul@daishisystems.com>
2016-10-27 23:33:31 +0100

    Added fan-out event-publishing (across *partitions).

--- Daishi.Pluralsight.EventHub.WebTrafficEmulator/Program.cs
+++ Daishi.Pluralsight.EventHub.WebTrafficEmulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,22 @@ namespace Daishi.Pluralsight.EventHub.WebTrafficEmulator
 {
     internal static class Program
     {
+        private static List<string> _partitionKeys;
+
         private static void Main(string[] args)
         {
             Console.WriteLine("Press Ctrl-C to stop the sender process");
             Console.WriteLine("Press Enter to start now");
             Console.ReadLine();
+
+            _partitionKeys = new List<string>
+            {
+                "17DF2B60-2F85-43D3-9476-9A66F2506406",
+                "B9D8A345-36B5-4181-AAE4-29794ADEA4A0",
+                "2D5FF03A-E730-4476-8429-DC7F9546A560",
+                "123C8798-1E5E-4FDD-BCC7-8032E7A06739"
+            };
+
             SendRandomApplicationMetadataEvents();
         }
 
@@ -44,12 +56,20 @@ namespace Daishi.Pluralsight.EventHub.WebTrafficEmulator
         ///     <see cref="ApplicationMetadataEvent" /> is an event that contains metadata
         ///     originating from an imaginary up-stream web application.
         /// </param>
+        /// <param name="partitionKey">
+        ///     <see cref="partitionKey" /> is the Event Hub Partition Key that determines
+        ///     the Event Hub Partition to which
+        ///     <see cref="applicationMetadataEvent" /> will be sent.
+        /// </param>
         private static void SendApplicationMetadata(EventHubClient eventHubClient,
-            ApplicationMetadataEvent applicationMetadataEvent)
+            ApplicationMetadataEvent applicationMetadataEvent, string partitionKey)
         {
             var serialisedApplicationMetadata = JsonConvert.SerializeObject(applicationMetadataEvent);
 
-            var eventData = new EventData(Encoding.UTF8.GetBytes(serialisedApplicationMetadata));
+            var eventData = new EventData(Encoding.UTF8.GetBytes(serialisedApplicationMetadata))
+            {
+                PartitionKey = partitionKey
+            };
 
             eventHubClient.Send(eventData);
         }
@@ -79,36 +99,39 @@ namespace Daishi.Pluralsight.EventHub.WebTrafficEmulator
             var random = new Random();
             var values = Enum.GetValues(typeof (Device));
 
-            const int eventCount = 100;
+            const int eventCount = 25;
 
-            for (var i = 0; i < eventCount; i++)
+            foreach (var partitionKey in _partitionKeys)
             {
-                try
+                for (var j = 0; j < eventCount; j++)
                 {
-                    var applicationMetadata = new ApplicationMetadataEvent
+                    try
                     {
-                        IPAddress = GenerateRandomIPAddress(random),
-                        Time = DateTime.UtcNow,
-                        Device = (Device) values.GetValue(random.Next(1, values.Length))
-                    };
-                    // todo: Split into groups of 25, each with a specific Partition Key.
-                    SendApplicationMetadata(eventHubClient, applicationMetadata);
+                        var applicationMetadata = new ApplicationMetadataEvent
+                        {
+                            IPAddress = GenerateRandomIPAddress(random),
+                            Time = DateTime.UtcNow,
+                            Device = (Device) values.GetValue(random.Next(1, values.Length))
+                        };
 
-                    Console.WriteLine("Sent event: {0}", applicationMetadata);
-                }
-                catch (Exception exception)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
-                    Console.ResetColor();
+                        SendApplicationMetadata(eventHubClient, applicationMetadata, partitionKey);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("{0} > Exception: {1}", DateTime.Now, exception.Message);
+                        Console.ResetColor();
+                    }
+
+                    Task.Delay(10);
                 }
 
-                Task.Delay(10);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Sent {0} events to {1}.", eventCount, partitionKey);
+                Console.ResetColor();
             }
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Sent {0} events.", eventCount);
-            Console.ResetColor();
+            Console.ReadLine();
         }
     }
 }
